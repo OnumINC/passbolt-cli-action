bab6e82745054af77427d68e7a04a0665648d774
Antonio Huete Jimenez <antonio.huete@onum.com>
2024-06-12 12:28:17 +0200

    feat: add basic logic to entrypoint (2)

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,31 +6,31 @@ PASSBOLT_CLI="/usr/local/bin/go-passbolt-cli"
 
 if [[ -z "${INPUTS_ARGS}" ]]; then
 	echo "ERROR! No args were provided."
-  exit 255
+	exit 255
 fi
 
 if [[ -z "${INPUTS_PASSBOLT_URL}" ]]; then
 	echo "ERROR! No passbolt URL was provided."
-  exit 255
+	exit 255
 fi
 
 if [[ -z "${INPUTS_PASSWORD}" ]]; then
 	echo "ERROR! No user password was provided."
-  exit 255
+	exit 255
 fi
 
 if [[ -z "${INPUTS_PRIVATEKEY}" ]]; then
 	echo "ERROR! No user private key string was provided."
-  exit 255
+	exit 255
 fi
 
 # Configure passbolt CLI
-${PASSBOLT_CLI} --serverAddress "${INPUTS_PASSBOLT_URL}" --userPassword "${INPUTS_PASSWORD}" --userPrivateKey "${INPUTS_PRIVATEKEY}" > /dev/null 2>&1
+${PASSBOLT_CLI} --serverAddress "${INPUTS_PASSBOLT_URL}" --userPassword "${INPUTS_PASSWORD}" --userPrivateKey "${INPUTS_PRIVATEKEY}" >/dev/null 2>&1
 
 if [ $? -ne 0 ]; then
-  echo "ERROR: Failed to configure passbolt CLI"
-  exit 255
+	echo "ERROR: Failed to configure passbolt CLI"
+	exit 255
 fi
 
 # Call cli with args
-${PASSBOLT_CLI} "${args[@]}"
+${PASSBOLT_CLI} "${INPUT_ARGS}"
