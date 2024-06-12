a94316611913f311eb93a6bb31f49d645c2b0ecf
Antonio Huete Jimenez <tuxillo@quantumachine.net>
2024-06-12 10:44:01 +0000

    feat: Add entrypoint (#1)
    feat: Add basic entrypoint handling.
    
    Signed-off-by: Antonio Huete <tuxillo@quantumachine.net>

---
+++ entrypoint.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+
+set -e
+
+PASSBOLT_CLI="/usr/local/bin/go-passbolt-cli"
+
+if [[ -z "${INPUT_ARGS}" ]]; then
+	echo "ERROR! No args were provided."
+	exit 255
+fi
+
+if [[ -z "${INPUT_PASSBOLT_URL}" ]]; then
+	echo "ERROR! No passbolt URL was provided."
+	exit 255
+fi
+
+if [[ -z "${INPUT_PASSWORD}" ]]; then
+	echo "ERROR! No user password was provided."
+	exit 255
+fi
+
+if [[ -z "${INPUT_PRIVATEKEY}" ]]; then
+	echo "ERROR! No user private key string was provided."
+	exit 255
+fi
+
+# Configure passbolt CLI
+${PASSBOLT_CLI} --serverAddress "${INPUT_PASSBOLT_URL}" --userPassword "${INPUT_PASSWORD}" --userPrivateKey "${INPUT_PRIVATEKEY}" >/dev/null 2>&1
+
+if [ $? -ne 0 ]; then
+	echo "ERROR: Failed to configure passbolt CLI"
+	exit 255
+fi
+
+# Call cli with args
+${PASSBOLT_CLI} "${INPUT_ARGS}"
